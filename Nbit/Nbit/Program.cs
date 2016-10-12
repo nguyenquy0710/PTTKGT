fc41a187f6844057d5d3a3146c52784c1f996671
nguyenquy0710 <A1s2d3f4g5h6j7k8l9>
2016-10-12 09:03:55 +0700

    Edit Nbit

--- Nbit/Nbit/Program.cs
+++ Nbit/Nbit/Program.cs
@@ -8,8 +8,36 @@ namespace Nbit
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int n;
+        public static int[] A;
+        public static void tryBit(int i)
         {
+            for (int j = 0; j <= 1; j++)
+            {
+                A[i] = j;
+                if (i == n - 1)
+                {
+                    PrintSolution();
+                }
+                else
+                    tryBit(i + 1);
+            }
+        }
+
+        private static void PrintSolution()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(A[i]);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public static void Main(string[] args)
+        {
+            n = 4;
+            A = new int[n];
+            tryBit(0);
         }
     }
 }
